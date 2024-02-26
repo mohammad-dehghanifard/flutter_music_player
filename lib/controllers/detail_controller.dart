673ecaf8a63420cdae4e277ae0b79421e936ef81
mohammad <m.deheghanifard@gmail.com>
2024-02-26 11:31:06 +0330

    Implementation of the AudioDetailController class to control the music playback of the detail page

---
+++ lib/controllers/detail_controller.dart
@@ -0,0 +1,66 @@
+import 'package:get/get.dart';
+import 'package:just_audio/just_audio.dart';
+
+class AudioDetailController extends GetxController {
+  final Uri uri;
+  AudioDetailController(this.uri);
+//============================= Variables ======================================
+  final AudioPlayer audioPlayer = AudioPlayer();
+  Duration currentDuration = const Duration();
+  Duration? duration = const Duration();
+
+//============================= Methods ========================================
+
+  Future<void> _getAudio() async {
+   duration = await audioPlayer.setAudioSource(AudioSource.uri(uri));
+    audioPlayer.positionStream.listen((event) {
+      currentDuration = event;
+      update();
+    });
+    update();
+  }
+
+  void playAndStopAudio() {
+    if(audioPlayer.playing) {
+      audioPlayer.stop();
+      update();
+    } else {
+      audioPlayer.play();
+      update();
+    }
+  }
+
+  String _formatDuration(Duration duration) {
+    String twoDigits(int n) => n.toString().padLeft(2, "0");
+    String twoDigitMinutes = twoDigits(duration.inMinutes.remainder(60));
+    String twoDigitSeconds = twoDigits(duration.inSeconds.remainder(60));
+    return "$twoDigitMinutes:$twoDigitSeconds";
+  }
+
+  String getDuration(){
+    return _formatDuration(duration!);
+  }
+
+  String getCurrentDuration(){
+    return _formatDuration(currentDuration);
+  }
+
+  double getSliderValue(){
+    return  currentDuration.inSeconds / (duration?.inSeconds ?? 1);
+  }
+
+//============================= Life Cycles ====================================
+  @override
+  void onInit() {
+    _getAudio();
+    super.onInit();
+  }
+
+  @override
+  void onClose() {
+    audioPlayer.dispose();
+    super.onClose();
+  }
+
+
+}
--- lib/views/song_detail_view.dart
+++ lib/views/song_detail_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_music_player/controllers/detail_controller.dart';
 import 'package:flutter_music_player/widgets/detail_page_app_bar_widget.dart';
 import 'package:get/get.dart';
 import 'package:on_audio_query/on_audio_query.dart';
@@ -13,69 +14,96 @@ class SongDetailView extends StatelessWidget {
     final size = MediaQuery.sizeOf(context);
     return Scaffold(
       body: SafeArea(
-          child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          //app bar
-          const DetailViewAppBar(),
-          SizedBox(height: size.height * 0.08),
-          // image
-          QueryArtworkWidget(
-              id: song.id,
-              type: ArtworkType.AUDIO,
-              artworkBorder: BorderRadius.circular(12),
-              artworkHeight: size.height / 4,
-              artworkWidth: size.height / 4),
-          SizedBox(height: size.height * 0.02),
-          // title
-          Text(song.title,
-              maxLines: 1,
-              style: context.textTheme.titleSmall,
-              overflow: TextOverflow.ellipsis),
-          // artist name
-          Text(
-            song.artist ?? "نامی جهت نمایش یافت نشد...",
-            style: context.textTheme.labelSmall!
-                .apply(color: context.theme.colorScheme.primary),
-          ),
-          SizedBox(height: size.height * 0.02),
-          // slider
-          SliderTheme(
-              data: SliderThemeData(
-                activeTrackColor: context.theme.colorScheme.primary,
-                thumbColor: context.theme.colorScheme.onPrimary,
-                disabledActiveTrackColor: Colors.blueGrey,
-                inactiveTrackColor: context.theme.colorScheme.tertiary,
-                trackHeight: 2,
-                overlayColor:
-                    context.theme.colorScheme.onPrimary.withOpacity(0.10),
-                thumbShape: const RoundSliderThumbShape(enabledThumbRadius: 5),
-              ),
-              child: Slider(value: 0.4, onChanged: (value) {})),
-          // times
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: size.width * 0.07),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text("12:40", style: context.textTheme.bodySmall),
-                Text("12:40", style: context.textTheme.bodySmall),
-              ],
-            ),
-          ),
-          SizedBox(height: size.height * 0.02),
-          // icons
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              IconButton(onPressed: () {}, icon: const Icon(Icons.fast_rewind_rounded,size: 48)),
-              IconButton(onPressed: () {}, icon: const Icon(Icons.play_arrow,size: 48)),
-              IconButton(onPressed: () {}, icon: const Icon(Icons.fast_forward,size: 48))
-            ],
-          ),
-
-        ],
-      )),
+          child: GetBuilder<AudioDetailController>(
+              init: AudioDetailController(Uri.parse(song.uri!)),
+              builder: (controller) {
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    //app bar
+                    const DetailViewAppBar(),
+                    SizedBox(height: size.height * 0.08),
+                    // image
+                    QueryArtworkWidget(
+                        id: song.id,
+                        keepOldArtwork: true,
+                        type: ArtworkType.AUDIO,
+                        artworkBorder: BorderRadius.circular(12),
+                        artworkHeight: size.height / 4,
+                        artworkWidth: size.height / 4),
+                    SizedBox(height: size.height * 0.02),
+                    // title
+                    Text(song.title,
+                        maxLines: 1,
+                        style: context.textTheme.titleSmall,
+                        overflow: TextOverflow.ellipsis),
+                    // artist name
+                    Text(
+                      song.artist ?? "نامی جهت نمایش یافت نشد...",
+                      style: context.textTheme.labelSmall!
+                          .apply(color: context.theme.colorScheme.primary),
+                    ),
+                    SizedBox(height: size.height * 0.02),
+                    // slider
+                    SliderTheme(
+                        data: SliderThemeData(
+                          activeTrackColor: context.theme.colorScheme.primary,
+                          thumbColor: context.theme.colorScheme.onPrimary,
+                          disabledActiveTrackColor: Colors.blueGrey,
+                          inactiveTrackColor:
+                              context.theme.colorScheme.tertiary,
+                          trackHeight: 2,
+                          overlayColor: context.theme.colorScheme.onPrimary
+                              .withOpacity(0.10),
+                          thumbShape: const RoundSliderThumbShape(
+                              enabledThumbRadius: 5),
+                        ),
+                        child: Slider(
+                            value: controller.getSliderValue(),
+                            onChanged: (value) {
+                              controller.audioPlayer.seek(Duration(
+                                  seconds: (value *
+                                          (controller.duration?.inSeconds ?? 0))
+                                      .round()));
+                            })),
+                    // times
+                    Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: size.width * 0.07),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(controller.getDuration(),
+                              style: context.textTheme.bodySmall),
+                          Text(controller.getCurrentDuration(),
+                              style: context.textTheme.bodySmall),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: size.height * 0.02),
+                    // icons
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        IconButton(
+                            onPressed: () {},
+                            icon: const Icon(Icons.fast_rewind_rounded,
+                                size: 48)),
+                        IconButton(
+                            onPressed: () => controller.playAndStopAudio(),
+                            icon: Icon(
+                                controller.audioPlayer.playing
+                                    ? Icons.pause
+                                    : Icons.play_arrow,
+                                size: 48)),
+                        IconButton(
+                            onPressed: () {},
+                            icon: const Icon(Icons.fast_forward, size: 48))
+                      ],
+                    ),
+                  ],
+                );
+              })),
     );
   }
 }
