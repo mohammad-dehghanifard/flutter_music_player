df8b15e7f540d3164e8854c8e8c0bbdf09173c77
mohammad <m.deheghanifard@gmail.com>
2024-02-26 10:45:18 +0330

    completed SongDetailView ui

--- lib/themes/theme_helper.dart
+++ lib/themes/theme_helper.dart
@@ -7,13 +7,16 @@ class ThemeHelper {
     colorScheme: const ColorScheme.light(
       primary: Color(0xFF0B40FF),
       primaryContainer: Color(0xFF466FFF),
+      onPrimary: Color(0xFF466FFF),
+      tertiary: Color(0xFFC4C4C4),
     ),
     iconTheme: const IconThemeData(
       color: Color(0xFF0B40FF),
     ),
     textTheme: TextTheme(
       titleSmall: GoogleFonts.vazirmatn(fontSize : 16,fontWeight : FontWeight.w700,color : const Color(0xFF030E35)),
-      labelSmall: GoogleFonts.vazirmatn(fontSize : 12,fontWeight : FontWeight.w400,color : Colors.grey)
+      labelSmall: GoogleFonts.vazirmatn(fontSize : 12,fontWeight : FontWeight.w400,color : Colors.grey),
+      bodySmall: GoogleFonts.vazirmatn(fontSize : 12,fontWeight : FontWeight.w500),
     ),
     useMaterial3: true,
   );
@@ -22,7 +25,9 @@ class ThemeHelper {
     scaffoldBackgroundColor: const Color(0xFF030E35),
     colorScheme: const ColorScheme.dark(
       primary: Color(0xFF466FFF),
-      primaryContainer: Color(0xFF466FFF)
+      primaryContainer: Color(0xFF466FFF),
+      onPrimary: Color(0xFFFFFFFF),
+      tertiary: Color(0xFFC4C4C4),
     ),
     iconTheme: const IconThemeData(
       color: Color(0xFF466FFF),
@@ -30,7 +35,8 @@ class ThemeHelper {
     useMaterial3: true,
     textTheme: TextTheme(
         titleSmall: GoogleFonts.vazirmatn(fontSize : 16,fontWeight : FontWeight.w700,color :  Colors.white),
-        labelSmall: GoogleFonts.vazirmatn(fontSize : 12,fontWeight : FontWeight.w400,color : Colors.grey)
+        labelSmall: GoogleFonts.vazirmatn(fontSize : 12,fontWeight : FontWeight.w400,color : Colors.grey),
+      bodySmall: GoogleFonts.vazirmatn(fontSize : 12,fontWeight : FontWeight.w500),
     ),
   );
 
--- lib/views/song_detail_view.dart
+++ lib/views/song_detail_view.dart
@@ -1,23 +1,81 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_music_player/widgets/detail_page_app_bar_widget.dart';
+import 'package:get/get.dart';
 import 'package:on_audio_query/on_audio_query.dart';
 
 class SongDetailView extends StatelessWidget {
   const SongDetailView({super.key, required this.song});
+
   final SongModel song;
+
   @override
   Widget build(BuildContext context) {
-    return  const Scaffold(
-      body: SafeArea(child: Column(
+    final size = MediaQuery.sizeOf(context);
+    return Scaffold(
+      body: SafeArea(
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           //app bar
-          DetailViewAppBar()
+          const DetailViewAppBar(),
+          SizedBox(height: size.height * 0.08),
+          // image
+          QueryArtworkWidget(
+              id: song.id,
+              type: ArtworkType.AUDIO,
+              artworkBorder: BorderRadius.circular(12),
+              artworkHeight: size.height / 4,
+              artworkWidth: size.height / 4),
+          SizedBox(height: size.height * 0.02),
+          // title
+          Text(song.title,
+              maxLines: 1,
+              style: context.textTheme.titleSmall,
+              overflow: TextOverflow.ellipsis),
+          // artist name
+          Text(
+            song.artist ?? "نامی جهت نمایش یافت نشد...",
+            style: context.textTheme.labelSmall!
+                .apply(color: context.theme.colorScheme.primary),
+          ),
+          SizedBox(height: size.height * 0.02),
+          // slider
+          SliderTheme(
+              data: SliderThemeData(
+                activeTrackColor: context.theme.colorScheme.primary,
+                thumbColor: context.theme.colorScheme.onPrimary,
+                disabledActiveTrackColor: Colors.blueGrey,
+                inactiveTrackColor: context.theme.colorScheme.tertiary,
+                trackHeight: 2,
+                overlayColor:
+                    context.theme.colorScheme.onPrimary.withOpacity(0.10),
+                thumbShape: const RoundSliderThumbShape(enabledThumbRadius: 5),
+              ),
+              child: Slider(value: 0.4, onChanged: (value) {})),
+          // times
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: size.width * 0.07),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("12:40", style: context.textTheme.bodySmall),
+                Text("12:40", style: context.textTheme.bodySmall),
+              ],
+            ),
+          ),
+          SizedBox(height: size.height * 0.02),
+          // icons
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              IconButton(onPressed: () {}, icon: const Icon(Icons.fast_rewind_rounded,size: 48)),
+              IconButton(onPressed: () {}, icon: const Icon(Icons.play_arrow,size: 48)),
+              IconButton(onPressed: () {}, icon: const Icon(Icons.fast_forward,size: 48))
+            ],
+          ),
+
         ],
       )),
     );
   }
-
 }
-
-
